d7951fe51dd47d2a91aa145fcadc0124fe9d31f7
Max Guthier <gutmaxhier@gmail.com>
2015-07-23 19:15:06 +0200

    RScript Mguthier for 2nd Prog Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##Create a placeholder variable for the inverse matrix
+  i <- NULL
+  ##Store the input matrix and any cached inverse matrices for reference in the cacheSolve function
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  setinverse <- function(inverse) i <<- inverse
+  ##Define the functions with which input matrix and inverse matrix are later retrieved
+  get <- function() x
+  getinverse <- function() i
+  ##Create a list with commands that refer to the specified matrices.
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##Check if an inverse matrix for the specified(!) input matrix has already been calculated
+  ## If yes return it and skip further calculations.
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  ##If the inverse matrix isn't cached already, create and return it
+  matrixobject <- x$get()
+  i <- solve(matrixobject, ...)
+  x$setinverse(i)
+  i
 }
+
+##Test the functions with a random 1000 * 1000 matrix
+a<-makeCacheMatrix(x<-matrix(sample(c(-1,0,1),1000000,replace=TRUE),1000,1000))
+IAmTheInverseMatrix<-cacheSolve(a)
